8a3be668dffd563c9de2551b58e35b580a36050f
Frank Montalvo <fmontalvo8a@hotmail.com>
2017-03-22 22:24:52 -0500

    Implemento Rederizacion
    Implemento Rederizacion

--- src/Graficos.java
+++ src/Graficos.java
@@ -1,6 +1,7 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 
 import javax.swing.JPanel;
 
@@ -16,6 +17,7 @@ public class Graficos extends JPanel implements FichaInterface {
 		int y = getHeight();
 
 		Graphics2D g2d = (Graphics2D) g;
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		g2d.setColor(Color.WHITE);
 
 		// Lineas Verticales///////////////////////
